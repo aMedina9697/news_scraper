e685bba2adbca899437f8060b8b549e8c6b3a1c0
aMedina9697 <aMedina9697@gmail.com>
2019-01-26 09:12:27 -0600

    uploading project

--- server.js
+++ server.js
@@ -1,48 +1,40 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const logger = require("morgan");
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require("body-parser");
+const path = require('path');
+const logger = require('morgan');
 const mongoose = require("mongoose");
 
-// Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Require all models
-var db = require("./models");
 
-var PORT = 3000;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 
-// Initialize Express
-var app = express();
+mongoose.Promise = Promise;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://alex:OrangE32@ds147411.mlab.com:47411/heroku_59fgdfg5"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+const db = mongoose.connection
 
-// Configure middleware
+db.on('error', err => console.log(`Mongoose connection error: ${err}`))
+// view engine setup
+db.once('open', () => console.log(`Connected to MongoDB`))
+ 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
 app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up promise with mongoose
-mongoose.Promise = global.Promise;
-// Connect to mongoDb
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines",
-  {
-    useMongoClient: true
-  }
-);
+const routes = require('./routes/index')
+app.use('/', routes)
 
 
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
